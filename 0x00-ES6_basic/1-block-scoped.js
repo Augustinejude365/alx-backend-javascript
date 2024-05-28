846ff6b60b5bb057b919bad57890085206b04a87
Augustinejude <divinefavour4jude@gmail.com>
2024-05-27 22:02:38 -0700

    Updated ES6

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  let task;
-  let task2;
+  const task = false;
+  const task2 = true;
+
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
+    if (task && task2) {
+      return [task, task2];
+    }
   }
-  task = false;
-  task2 = true;
+
   return [task, task2];
 }
