be070ceb9134653e853a4f13d2892c2f1dcbd8cb
Augustinejude <divinefavour4jude@gmail.com>
2024-07-25 15:53:26 -0700

    Skip testing

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -1,15 +1,35 @@
-const getPaymentTokenFromAPI = require('./6-payment_token');
 const { expect } = require('chai');
 
-describe('getPaymentTokenFromAPI', function () {
-  describe('arg true', function () {
-    it('resolved correctly', function (done) {
-      getPaymentTokenFromAPI(true)
-        .then((res) => {
-          expect(res).to.include({ data: 'Successful response from the API' });
-          done();
-        })
-        .catch((err) => done(err));
-    });
+describe('Testing numbers', () => {
+  it('1 is equal to 1', () => {
+    expect(1 === 1).to.be.true;
+  });
+
+  it('2 is equal to 2', () => {
+    expect(2 === 2).to.be.true;
+  });
+
+  it.skip('1 is equal to 3', () => {
+    expect(1 === 3).to.be.true;
+  });
+
+  it('3 is equal to 3', () => {
+    expect(3 === 3).to.be.true;
+  });
+
+  it('4 is equal to 4', () => {
+    expect(4 === 4).to.be.true;
+  });
+
+  it('5 is equal to 5', () => {
+    expect(5 === 5).to.be.true;
+  });
+
+  it('6 is equal to 6', () => {
+    expect(6 === 6).to.be.true;
+  });
+
+  it('7 is equal to 7', () => {
+    expect(7 === 7).to.be.true;
   });
 });
